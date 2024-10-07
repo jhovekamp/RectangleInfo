0e5d3e8596e94e9126d3d82ed8f1f9de377061fb
jhovekamp <knippjl@mail.uc.edu>
2024-10-07 01:45:15 -0400

    3.1

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,13 @@ public class Main
         double width = 0.0;
         double height = 0.0;
         double perimeter = 0.0;
+        double area = 0.0;
         double diagonal = 0.0;
         String trash = ""; // use for bad input which will read as a String
         boolean done = false;
         do
         {
-            System.out.print("Enter the width of the triangle: ");
+            System.out.print("Enter the width of the rectangle: ");
             if(in.hasNextDouble()) // OK safe to read in a double
             {
                 width = in.nextDouble();
@@ -32,10 +33,10 @@ public class Main
         // when done is true !done i.e. NOT TRUE is false
         do
         {
-            System.out.print("Enter the height of the triangle: ");
+            System.out.print("Enter the height of the rectangle: ");
             if(in.hasNextDouble()) // OK safe to read in a double
             {
-                width = in.nextDouble();
+                height = in.nextDouble();
                 in.nextLine(); // clears the newline from the buffer
                 done = true; // we got a valid number so we can end the loop
             }
@@ -49,9 +50,10 @@ public class Main
         }while(!done); // initially done is false so !done i.e. NOT FALSE, is true
         // when done is true !done i.e. NOT TRUE is false
         perimeter = 2*(height + width);
-        diagonal =((height * height) + (width * width));
+        area = height * width;
+        diagonal = Math.hypot(height,width);
         System.out.println("The perimeter of the rectangle with a width of " + width + " and a height of " + height + " is " + perimeter);
+        System.out.println("The area of the rectangle with a width of " + width + " and a height of " + height + " is " + area);
         System.out.println("The diagonal of the rectangle with a width of " + width + " and a height of " + height + " is " + diagonal);
-
     }
 }
